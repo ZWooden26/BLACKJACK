072a540a4b0be15acc2d83cf8fc23d6f4e7a57f5
ZWooden26 <m266984@usna.edu>
2023-11-13 11:46:08 -0500

    Update background.py

--- game/background.py
+++ game/background.py
@@ -14,7 +14,13 @@ def draw_background(screen):
     # initializing fonts
     title_font = pygame.font.Font('../Assets/Trocadero.ttf')
     text_font = pygame.font.Font('../Assets/Marlboro.ttf')
-    
+
+    # render text on-screen
+    title = title_font.render('BLACKJACK', True, (0, 0, 0))
+    instructions = text_font.render("'ENTER' to hit\n"
+                                    "'SPACE' to stay\n"
+                                    "")
+
     # blit center card stack
     for y in range(20):
         screen.blit(cardback, (WIDTH/2, HEIGHT/2 + y))
